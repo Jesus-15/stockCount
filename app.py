683b499ffe6561136bdde470b4de7b0efe4a5857
Oskar Papaj <oskarpapaj22@gmail.com>
2022-03-26 00:34:19 +0000

    test

--- app.py
+++ app.py
@@ -29,5 +29,20 @@ def inventory_read():
         return json_data
 
 
+@app.route('/api/add', methods=['GET', 'PUT'])
+def add_to_inventory():
+    if request.method == "GET":
+        position = int(request.args.get('number'))
+
+        with open('data/inventory.json', 'r') as file:
+            data = file.read()
+            json_data = json.loads(data)
+            current = json_data['inventory'][position-1]
+            item_data["count"] = author_to_delete
+            file.close()
+
+    # return json_data['inventory'][position]
+    return current["count"]
+
 if __name__ == '__main__':
     app.run()
